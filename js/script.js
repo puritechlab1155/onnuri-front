05f236f2438fd3c4184492a10cea5fecc0b6dbf5
Puritechlab <pub@puritechlab.com>
2025-01-20 16:09:58 +0900

    mob-active menu

--- js/script.js
+++ js/script.js
@@ -1,13 +1,53 @@
 document.addEventListener('DOMContentLoaded', function() {
 
     var header = document.querySelector('#header');
+
     if (header) {
         var offcanvasOpen = header.querySelector('.offcanvas-open');
+        var offcanvas = document.querySelector('.offcanvas');
         var headerLogoImg = header.querySelector('.logo-img');
 
+        // 오프캔버스 토글 이벤트 추가
         offcanvasOpen.addEventListener('click', function() {
             offcanvasOpen.classList.toggle('active');
+            offcanvas.classList.toggle('active'); // .offcanvas에 active 클래스 토글
+
+            // offcanvas의 transform 속성 변경
+            if (offcanvas.classList.contains('active')) {
+                offcanvas.style.transform = 'translateX(0%)'; // 메뉴 열기
+                // headerLogoImg.src = 'img/offcanvas-logo.png'; // 활성화 상태의 로고
+            } else {
+                offcanvas.style.transform = 'translateX(100%)'; // 메뉴 닫기
+                // headerLogoImg.src = 'img/avv_logo.png'; // 비활성화 상태의 로고
+
+                dropIconL.style.transform = 'rotate(-45deg)'; // 드롭 아이콘 원래대로
+                dropIconR.style.transform = 'rotate(45deg)';
+                gnbSub.style.display = 'none'; // 드롭다운 숨김
+            }
         });
+
+        // .gnb-training 클릭 시 .gnb-sub 드롭다운
+        var gnbTraining = document.querySelector('.gnb-mobile .gnb-training');
+        var gnbSub = document.querySelector('.gnb-mobile .gnb-sub');
+        var dropIconL= document.querySelector('.drop-arrow .drop-left');
+        var dropIconR= document.querySelector('.drop-arrow .drop-right');
+
+        if (gnbTraining && gnbSub) {
+            gnbTraining.addEventListener('click', function() {
+                gnbSub.classList.toggle('active'); // active 클래스 토글
+                
+                // 드롭다운 효과를 위해 display 속성을 변경
+                if (gnbSub.classList.contains('active')) {
+                    gnbSub.style.display = 'block'; // 드롭다운 표시
+                    dropIconL.style.transform = 'rotate(45deg)';
+                    dropIconR.style.transform = 'rotate(-45deg)';
+                } else {
+                    dropIconL.style.transform = 'rotate(-45deg)';
+                    dropIconR.style.transform = 'rotate(45deg)';
+                    gnbSub.style.display = 'none'; // 드롭다운 숨김
+                }
+            });
+        }
     }
 
     var home = document.querySelector('#home');
